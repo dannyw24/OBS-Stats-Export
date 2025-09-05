24c45d3b79e5052fde6cb932c89fc52f0cdd951b
dannyW24 <dannywardle24@gmail.com>
2025-09-05 21:55:25 +0100

    Initial commit: RuneLite OBS Stats Export plugin

--- src/main/java/com/example/ObsStatsExportPlugin.java
+++ src/main/java/com/example/ObsStatsExportPlugin.java
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class ObsStatsExportPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ObsStatsExportConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    ObsStatsExportConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ObsStatsExportConfig.class);
 	}
 }
